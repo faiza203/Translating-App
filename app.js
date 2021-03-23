4e72a3f9e07175f6936ad10fc99bccb75681d451
faiza203 <fiazfaiza981@gmail.com>
2021-03-24 10:55:27 +1345

    Create result.hbs

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 const http = require("http");
 const path = require("path");
-const { I18n } = require("i18n");
+const i18n = require("i18n");
 const express = require("express");
 const app = express();
 
 const port = 4000;
 
+i18n.configure({
+  locales: ["en", "ja", "ko"],
+  directory: path.join(__dirname, "/locales"),
+});
+
 var hbs = require("hbs");
 app.set("view engine", "hbs");
 
 app.get("/", function (req, res) {
+  i18n.setLocale("ja");
+  console.log(i18n.__("Hello"));
+  i18n.setLocale("ko");
+  console.log(i18n.__("Hello"));
+  res.render("index");
+});
+
+app.get("/en", function (req, res) {
+  i18n.setLocale("ja");
+  console.log(i18n.__("Hello"));
+  i18n.setLocale("ko");
+  console.log(i18n.__("Hello"));
+  res.render("index");
+});
+
+app.get("/ja", function (req, res) {
+  i18n.setLocale("ja");
+  console.log(i18n.__("Hello"));
+  i18n.setLocale("ko");
+  console.log(i18n.__("Hello"));
+  res.render("index");
+});
+app.get("/ko", function (req, res) {
+  i18n.setLocale("ja");
+  console.log(i18n.__("Hello"));
+  i18n.setLocale("ko");
+  console.log(i18n.__("Hello"));
   res.render("index");
 });
 
